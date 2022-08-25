08110c72f77e0372999b25e540806c3921f645d6
Vladislav Voitov <dobrihlopez@gmail.com>
2022-08-25 13:37:04 +0300

    refactor(GameplayFragment): apply dataBinding that observes viewModel

--- app/src/main/java/com/voitov/brainlevelup/presentation/BindingAdapters.kt
+++ app/src/main/java/com/voitov/brainlevelup/presentation/BindingAdapters.kt
@@ -1,10 +1,19 @@
 package com.voitov.brainlevelup.presentation
 
+import android.content.Context
+import android.content.res.ColorStateList
+import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.voitov.brainlevelup.R
 import com.voitov.brainlevelup.domain.entities.GameResults
+
+interface OnOptionClickListener {
+    fun onOptionClick(option: Int)
+}
 
 @BindingAdapter("emojiResult")
 fun bindEmojiResult(imageView: ImageView, isWinner: Boolean) {
@@ -48,6 +57,50 @@ fun bindScorePercentage(textView: TextView, gameResults: GameResults) {
         textView.context.getString(R.string.score_percentage),
         getScorePercentage(gameResults).toString()
     )
+}
+
+@BindingAdapter("minProgressToWin")
+fun bindMinProgressToWin(progressBar: ProgressBar, progress: Int) {
+    progressBar.secondaryProgress = progress
+}
+
+@BindingAdapter("progressOfCorrectAnswers")
+fun bindProgressOfCorrectAnswers(progressBar: ProgressBar, progress: Int) {
+    progressBar.setProgress(progress, true)
+}
+
+@BindingAdapter("enoughCorrectAnswersToWin")
+fun bindEnoughCorrectAnswersToWin(textView: TextView, isEnough: Boolean) {
+    textView.setTextColor(getColorResIdByState(textView.context, isEnough))
+}
+
+@BindingAdapter("enoughPercentageToWin")
+fun bindEnoughPercentageToWin(progressBar: ProgressBar, isEnough: Boolean) {
+    val colorResId = getColorResIdByState(progressBar.context, isEnough)
+    progressBar.progressTintList =
+        ColorStateList.valueOf(colorResId)
+}
+
+@BindingAdapter("integerAsText")
+fun bindIntegerAsText(textView: TextView, number: Int) {
+    textView.text = number.toString()
+}
+
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, optionClickListener: OnOptionClickListener) {
+    textView.setOnClickListener {
+        val number = textView.text.toString().toInt()
+        optionClickListener.onOptionClick(number)
+    }
+}
+
+private fun getColorResIdByState(context: Context, enough: Boolean): Int {
+    val colorResId = if (enough) {
+        android.R.color.holo_green_light
+    } else {
+        android.R.color.holo_red_light
+    }
+    return ContextCompat.getColor(context, colorResId)
 }
 
 private fun getScorePercentage(gameResults: GameResults) =
--- app/src/main/java/com/voitov/brainlevelup/presentation/GameplayFragment.kt
+++ app/src/main/java/com/voitov/brainlevelup/presentation/GameplayFragment.kt
@@ -1,11 +1,9 @@
 package com.voitov.brainlevelup.presentation
 
-import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -18,17 +16,6 @@ class GameplayFragment : Fragment() {
     private var _viewBinding: FragmentGameplayBinding? = null
     private val viewBinding: FragmentGameplayBinding
         get() = _viewBinding ?: throw RuntimeException("viewBinding in GameplayFragment is null")
-
-    private val textViewOptions by lazy {
-        arrayOf(
-            viewBinding.textViewOption1,
-            viewBinding.textViewOption2,
-            viewBinding.textViewOption3,
-            viewBinding.textViewOption4,
-            viewBinding.textViewOption5,
-            viewBinding.textViewOption6,
-        )
-    }
 
     private val args by navArgs<GameplayFragmentArgs>()
 
@@ -51,9 +38,9 @@ class GameplayFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        setupViewBinding()
         setupViewModelObservers()
-        setupClickListeners()
+        //setupClickListeners()
     }
 
     override fun onDestroyView() {
@@ -61,55 +48,15 @@ class GameplayFragment : Fragment() {
         _viewBinding = null
     }
 
-    private fun setupClickListeners() {
-        for (textViewOption in textViewOptions) {
-            textViewOption.setOnClickListener {
-                viewModel.selectAnswer(textViewOption.text.toString().toInt())
-            }
-        }
+    private fun setupViewBinding() {
+        viewBinding.viewModel = viewModel
+        viewBinding.lifecycleOwner = viewLifecycleOwner
     }
 
     private fun setupViewModelObservers() {
-        viewModel.generateQuestion.observe(viewLifecycleOwner) {
-            for (i in 0 until textViewOptions.size) {
-                textViewOptions[i].text = it.options[i].toString()
-            }
-            viewBinding.textViewSum.text = it.sum.toString()
-            viewBinding.textViewLeftNumber.text = it.visibleValue.toString()
-        }
-        viewModel.formattedTime.observe(viewLifecycleOwner) {
-            viewBinding.textViewTimer.text = it
-        }
-        viewModel.progressAnswers.observe(viewLifecycleOwner) {
-            viewBinding.textViewProgressAnswer.text = it
-        }
-        viewModel.minPercentageToWin.observe(viewLifecycleOwner) {
-            viewBinding.progressBarAnswers.secondaryProgress = it
-        }
-        viewModel.percentageOfCorrectAnswers.observe(viewLifecycleOwner) {
-            viewBinding.progressBarAnswers.setProgress(it, true)
-        }
-        viewModel.enoughCorrectAnswersToWin.observe(viewLifecycleOwner) {
-            viewBinding.textViewProgressAnswer.setTextColor(getColorResIdByState(it))
-        }
-        viewModel.enoughPercentageToWin.observe(viewLifecycleOwner) {
-            val colorResId = getColorResIdByState(it)
-            viewBinding.progressBarAnswers.progressTintList =
-                ColorStateList.valueOf(colorResId)
-        }
-
         viewModel.gameResults.observe(viewLifecycleOwner) {
             launchGameResultsFragment(it)
         }
-    }
-
-    private fun getColorResIdByState(enough: Boolean): Int {
-        val colorResId = if (enough) {
-            android.R.color.holo_green_light
-        } else {
-            android.R.color.holo_red_light
-        }
-        return ContextCompat.getColor(requireContext(), colorResId)
     }
 
     private fun launchGameResultsFragment(gameResults: GameResults) {
